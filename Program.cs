2fcebee8d15a8ea5185568e0977b71cae30986e6
Martin Davis <martin_davis3@yahoo.ca>
2020-02-12 11:24:50 -0700

    completed if block

--- Program.cs
+++ Program.cs
@@ -6,8 +6,17 @@ namespace creview
     {
         static void Main(string[] args)
         {
-            int x =20;
-            Console.WriteLine("Hello World!" + x);
+            int x =35;
+            Console.WriteLine("Hello Martin!" + x);
+
+            if (x < 30) {
+                Console.WriteLine("x is less than thirty!");
+
+            } else if (x == 30) {
+                Console.WriteLine("x is thirty!");
+            } else {
+                Console.WriteLine("x is greater than Thirty!");
+            }
         }
     }
 }
