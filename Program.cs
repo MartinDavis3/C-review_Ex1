58583d098d6e89052f7a713e0897cdfe8eef89a0
Martin Davis <martin_davis3@yahoo.ca>
2020-02-12 11:16:55 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace creview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x =20;
+            Console.WriteLine("Hello World!" + x);
+        }
+    }
+}
